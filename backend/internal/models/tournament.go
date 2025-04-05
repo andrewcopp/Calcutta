e4bfa447c3e8a217c38f8008e49eb87e728fd3ad
prelude-andrew <114695256+prelude-andrew@users.noreply.github.com>
2025-04-04 23:11:11 -0500

    Add Rounds field to Tournament model

--- backend/internal/models/tournament.go
+++ backend/internal/models/tournament.go
@@ -2,6 +2,7 @@ package models
 
 // Tournament represents a basketball tournament in the real world
 type Tournament struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID     string `json:"id"`
+	Name   string `json:"name"`
+	Rounds int    `json:"rounds"` // Total number of rounds in the tournament
 }
